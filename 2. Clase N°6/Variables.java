e4828b966ef84881e4d22368b463f5cf9e72d01e
Collado Claudio <46108725+ccollado7@users.noreply.github.com>
2019-08-21 16:19:27 -0300

    Add files via upload

---
+++ 2. Clase N°6/Variables.java
@@ -0,0 +1,23 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		byte edad; //Reservo un espacio en memoria para almacenar el numero 35
+		
+		edad=35; //Inicializo la variable
+		
+		byte edad1=40; //Reservo un espacio en memoria e inicializo la misma en la misma linea
+		
+		// Los espacios verticales que dejemos en el codigo NO tiene relevancia en la ejecucion del codigo
+		
+		System.out.println(edad); //Imprime en consola el valor de edad en esta etapa del programa
+		System.out.println(edad1); //Imprime en consola el valor de edad1 en esta etapa del programa
+		
+		edad1=89; //Modifico el valor de la variable edad1 despues del la primera impresion y lo vuelvo a imprimir. Importante ver como es el flujo de ejecucion del programa
+				
+		System.out.println(edad1); //Imprime en consola el valor de edad1 en esta etapa del programa
+
+	}
+
+}
