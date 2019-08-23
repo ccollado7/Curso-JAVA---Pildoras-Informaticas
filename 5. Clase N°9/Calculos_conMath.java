196112824a3e99c1631665f92c99e0f506f917da
Collado Claudio <46108725+ccollado7@users.noreply.github.com>
2019-08-23 13:03:35 -0300

    Add files via upload

---
+++ 5. Clase N°9/Calculos_conMath.java
@@ -0,0 +1,16 @@
+
+public class Calculos_conMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Utilizo la Clase Math con el metodo sqrt que me permite calcular la raiz cuadrada de un numero
+		
+		
+		double raiz=Math.sqrt(9); // En la ayuda del metodo sqrt se indica que se devuelve un numero tipo double, por lo tanto NO puede colocar tipo int por que dara error
+		
+		System.out.println(raiz);
+		
+	}
+
+}
